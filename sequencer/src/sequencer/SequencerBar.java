eb78a7bafa1cbb94d76fd82e0365b44a98006bcf
Joachim Haensel <joachim.haensel@posteo.de>
2017-11-12 17:15:22 +0100

    initial (huge) commit. better now than never

---
+++ sequencer/src/sequencer/SequencerBar.java
@@ -0,0 +1,304 @@
+package sequencer;
+
+import java.awt.Rectangle;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.ShortMessage;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+import processing.event.MouseEvent;
+import sequencer.SequencerMain.InputState;
+import sequencer.SequencerMain.InputStateType;
+import sequencer.SequencerMain.PlayStatus;
+import sequencer.SequencerMain.SeqButton;
+
+public class SequencerBar
+{
+    private PApplet _p;
+    private PVector _insets;
+    private float _width;
+    private float _height;
+    private PVector _corner;
+    private float _buttonHeight;
+    private float _buttonWidth;
+    private int _inactiveColor;
+    private int _activeColor;
+    private int _beatColor;
+    private float _controlsWidth;
+    private MuteButton _muteButton;
+    
+    public enum MidiInstrumentType
+    {
+        UNKNOWN, SINGLE_CHANNEL_MULTIPLE_INSTRUMENTS, MULTIPLE_CHANNEL_MULTIPLE_INSTRUMENTS
+    }
+
+    private static final int INACTIVE_SYMBOL = -1; //no note/instrument
+    private static final int MAX_EVENTS_AT_SAME_TIME = 10; //maximum amount of parallel midi events memorized by this object
+
+    private int _steps;
+    private int _numTracks;
+    private int _stepsPerBeat;
+    private int[][] _activeSteps;
+    private MidiDevice _midiDevice;
+    private int _activeSubTrack;
+    private int _activeNote;
+    private MidiInstrumentType _trackMidiInstrumentType;
+    private int _activeChannel;
+    private int _currentStep;
+    private int _currentMaxSteps;
+    private InputState _inputState;
+
+    public SequencerBar(PVector corner, PVector insets, float areaWidth, float areaHeight, int steps, int numTracks, int stepsPerBeat, SequencerMain processingApp)
+    {
+        _p = processingApp;
+        _insets = insets;
+        _width = areaWidth;
+        _height = areaHeight;
+        _corner = corner;
+        _buttonHeight = (_height/numTracks)  - 2 * _insets.y;
+        _controlsWidth = 60;
+        _buttonWidth = (_width - 2 * _insets.x - _controlsWidth )/steps;
+        _inactiveColor = 255;
+        _activeColor = 32;
+        _beatColor = 128;
+        _muteButton = new MuteButton(processingApp, processingApp, new Rectangle((int)(_corner.x + insets.x), (int)(_corner.y + insets.y), 40, 40), null, null);
+        _inputState = processingApp.getInputState();
+
+        _steps = steps;
+        _numTracks = numTracks;
+        _stepsPerBeat = stepsPerBeat;
+        _activeSubTrack = 0;
+        _activeNote = 36;
+        _activeChannel = 0;
+        _currentStep = 0;
+        _currentMaxSteps = steps;
+        _activeSteps = new int[MAX_EVENTS_AT_SAME_TIME][steps]; 
+        for(int curNC = 0; curNC < MAX_EVENTS_AT_SAME_TIME; curNC++)
+        {
+            for (int stepIdx = 0; stepIdx < _activeSteps[curNC].length; stepIdx++)
+            {
+                _activeSteps[curNC][stepIdx] = INACTIVE_SYMBOL; //no note/instrument
+            }
+        }
+        _trackMidiInstrumentType = MidiInstrumentType.UNKNOWN;
+    }
+
+    public float getHeight()
+    {
+        return _buttonHeight + 2 * _insets.y;
+    }
+
+    public Rectangle getDimensions()
+    {
+        return new Rectangle((int)_corner.x, (int)_corner.y, (int)_width, (int)getHeight());
+    }
+
+    public void draw(int beat)
+    {
+        int prevCol = _p.getGraphics().fillColor;
+        _p.fill(_inactiveColor);
+        for(int stepIdx = 0; stepIdx < _steps; stepIdx++)
+        {
+            if(stepIdx != _currentStep)
+            {
+                if(_activeSteps[_activeSubTrack][stepIdx] != INACTIVE_SYMBOL)
+                {
+                    _p.fill(0, 0, 255);
+                }
+                else 
+                {
+                    if (stepIdx % _stepsPerBeat == 0)
+                    {
+                        _p.fill(_beatColor);
+                        if(stepIdx == _currentMaxSteps)
+                        {
+                            _p.fill(128, 0, 255);
+                        }
+                    }
+                    else
+                    {
+                        _p.fill(_inactiveColor);
+                        if(stepIdx == _currentMaxSteps)
+                        {
+                            _p.fill(128, 0, 255);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                _p.fill(_activeColor);
+            }
+            _p.rect(stepIdx * _buttonWidth + _insets.x + _corner.x + _controlsWidth, _insets.y + _corner.y, _buttonWidth, _buttonHeight);
+        }
+        _p.fill(prevCol);
+        _muteButton.draw();
+    }
+
+    public void mousePressed(MouseEvent event, InputState inputState) 
+    {
+        _muteButton.mousePressed(event, inputState);
+        int mouseY = event.getY();
+        float cornerY = _corner.y + _insets.y;
+        if(mouseY > cornerY && mouseY < (cornerY + _buttonHeight))
+        {
+            int activatedButton = getClickedButtonIdx(event);
+            if(activatedButton != -1)
+            {
+                switch (_inputState.getState())
+                {
+                    case REGULAR:
+                        activateNote(activatedButton);
+                        break;
+                    case STEP_LENGTH_SELECT_ENABLED:
+                        setNewMaxSteps(inputState, activatedButton);
+                        break;
+                    default:
+                        break;
+                }
+                draw(0);
+            }
+        }
+    }
+
+    private void setNewMaxSteps(InputState inputState, int activatedButton)
+    {
+        _currentMaxSteps = activatedButton + 1;
+        inputState.maxStepsSet();
+        System.out.println("max steps " + _currentMaxSteps);
+    }
+
+    private void activateNote(int activatedButton)
+    {
+        if(_activeSteps[_activeSubTrack][activatedButton] == INACTIVE_SYMBOL)
+        {
+            _activeSteps[_activeSubTrack][activatedButton] = _activeNote;
+        }
+        else
+        {
+            _activeSteps[_activeSubTrack][activatedButton] = INACTIVE_SYMBOL;
+        }
+    }
+
+    private int getClickedButtonIdx(MouseEvent event)
+    {
+        int activatedButton = -1;
+        float offset = _insets.x + _corner.x + _controlsWidth;
+        for(int cnt = 0; cnt < _steps; cnt++)
+        {
+            int mouseX = event.getX();
+            if(mouseX > cnt * _buttonWidth + offset)
+            {
+                if(mouseX < (cnt + 1) * _buttonWidth + offset)
+                {
+                   activatedButton = cnt;
+                   break;
+                }
+            }
+        }
+        return activatedButton;
+    }
+
+    public void sendAdvance(int currentStep)
+    {
+        _currentStep++;
+        if(_currentStep >= _currentMaxSteps)
+        {
+            _currentStep = 0;
+        }
+        if((_activeSteps[_activeSubTrack][_currentStep] != INACTIVE_SYMBOL) && _muteButton.isNotSet())
+        {
+            ShortMessage midiMsg = new ShortMessage();
+            try
+            {
+                switch (_trackMidiInstrumentType)
+                {
+                    case SINGLE_CHANNEL_MULTIPLE_INSTRUMENTS:
+                        midiMsg.setMessage(ShortMessage.NOTE_ON, 0, _activeNote, 127);
+                        break;
+                    case MULTIPLE_CHANNEL_MULTIPLE_INSTRUMENTS:
+                        midiMsg.setMessage(ShortMessage.NOTE_ON, _activeChannel, _activeNote, 127);
+                        break;
+                    default:
+                        return;
+                }
+                Receiver receiver = _midiDevice.getReceiver();
+                receiver.send(midiMsg, -1);
+            }
+            catch (InvalidMidiDataException exc)
+            {
+                exc.printStackTrace();
+            }
+            catch (MidiUnavailableException exc)
+            {
+                exc.printStackTrace();
+            }
+        }
+    }
+    
+    public void sendStopped()
+    {
+        _currentStep = 0;
+    }
+
+    public void setMidiDevice(MidiDevice midiDevice)
+    {
+        _midiDevice = midiDevice;
+    }
+
+    public void setActiveNote(int note)
+    {
+        _trackMidiInstrumentType = MidiInstrumentType.SINGLE_CHANNEL_MULTIPLE_INSTRUMENTS;
+        _activeNote = note;
+    }
+
+    public void setActiveChannel(int channel)
+    {
+        _trackMidiInstrumentType = MidiInstrumentType.MULTIPLE_CHANNEL_MULTIPLE_INSTRUMENTS;
+        _activeChannel = channel;
+    }
+    
+    public void setCurrentMaxSteps(int maxSteps)
+    {
+        _currentMaxSteps = maxSteps;
+    }
+
+    public class MuteButton extends SeqButton
+    {
+        private boolean _isMuted;
+
+        public MuteButton(SequencerMain sequencerMain, PApplet mainApp, Rectangle area, PlayStatus playStatus, InputState inputState)
+        {
+            sequencerMain.super(mainApp, area, playStatus, inputState);
+            _isMuted = false;
+        }
+
+        public boolean isNotSet()
+        {
+            return !_isMuted;
+        }
+
+        @Override
+        protected void buttonPressed(InputState inputState)
+        {
+            _isMuted = !_isMuted;
+        }
+
+        @Override
+        protected void setColor()
+        {
+            if(_isMuted)
+            {
+                _mainApp.fill(255, 0, 0);
+            }
+            else
+            {
+                _mainApp.fill(128);
+            }
+        }
+    }
+}
---
+++ sequencer/src/sequencer/SequencerMain.java
@@ -0,0 +1,478 @@
+package sequencer;
+
+import java.awt.Rectangle;
+
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiDevice.Info;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PVector;
+import processing.event.MouseEvent;
+import sequencer.SequencerMain.InputState;
+
+public class SequencerMain extends PApplet
+{
+    private static final int STEPS_PER_BEAT = 4;
+    private static final int STEPS = 32;
+    private static final int NUM_TRACKS = 8;
+    
+    private long _stepTimer;
+    private long _drawTimer;
+    private long _passedTime;
+    private int _beatsPerMinute;
+    private int _currentStep;
+    private PFont _f;
+    private int _oldTime;
+    private int _millisToPass;
+    private MidiDevice _midiOut1;
+    private MidiDevice _midiOut2;
+    private SequencerBarArea _sequencerBarsArea;
+    private PlayButton _playButton;
+    private PlayStatus _playStatus;
+    private StopButton _stopButton;
+    private InputState _inputState;
+    private StepLengthSelectButton _stepLengthSelectButton;
+
+    public static void main(String[] args)
+    {
+        PApplet.main("sequencer.SequencerMain");
+    }
+    
+    @Override
+    public void settings()
+    {
+        size(1366, 768);
+//        fullScreen();
+    }
+
+    @Override
+    public void setup()
+    {
+        _stepTimer = 0;
+        _passedTime = 0;
+        _beatsPerMinute = 140;
+        _oldTime = 0;
+        _currentStep = 0;
+        _millisToPass = 60000 / (_beatsPerMinute * STEPS_PER_BEAT);
+
+        _f = createFont("Arial", 48, true);
+
+        _inputState = new InputState();
+        _inputState.setState(InputStateType.REGULAR);
+        
+        _playStatus = new PlayStatus(PlayStatus.STOPPED);
+        _sequencerBarsArea = new SequencerBarArea(this, new Rectangle(100, 10, width - 120, height - 100));
+        _playButton = new PlayButton(this, new Rectangle(width/2, height - 90, 80, 50), _playStatus, _inputState);
+        _playButton.draw();
+        _stopButton = new StopButton(this, new Rectangle(width/2 - 90, height - 90, 80, 50), _playStatus, _inputState);
+        _stopButton.draw();
+        _stepLengthSelectButton = new StepLengthSelectButton(this, new Rectangle(width/2 + 90, height - 90, 80, 50), _playStatus, _inputState);
+        mapMidi(_sequencerBarsArea);
+    }
+
+    private void mapMidi(SequencerBarArea sequencerBarsArea)
+    {
+        MidiDevice.Info[] deviceInfos = MidiSystem.getMidiDeviceInfo();
+        for (Info curDevice : deviceInfos)
+        {
+            System.out.print("Name: " + curDevice.getName());
+            System.out.print("  Description: " + curDevice.getDescription());
+            if(curDevice.getName().equals("USB Midi 4i4o") && curDevice.getDescription().equals("External MIDI Port"))
+            {
+                try
+                {
+                    _midiOut1 = MidiSystem.getMidiDevice(curDevice);
+                    _midiOut1.open();
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut1, 0);
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut1, 1);
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut1, 2);
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut1, 3);
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut1, 4);
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut1, 5);
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut1, 6);
+                }
+                catch (MidiUnavailableException exc)
+                {
+                    exc.printStackTrace();
+                }
+                System.out.print(" <---- SELECTED");
+            }
+            if(curDevice.getName().equals("MIDIOUT2 (USB Midi 4i4o)") && curDevice.getDescription().equals("External MIDI Port"))
+            {
+                try
+                {
+                    _midiOut2 = MidiSystem.getMidiDevice(curDevice);
+                    _midiOut2.open();
+                    sequencerBarsArea.setMidiDeviceBar(_midiOut2, 7);
+                }
+                catch (MidiUnavailableException exc)
+                {
+                    exc.printStackTrace();
+                }
+                System.out.print(" <---- SELECTED");
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    public void draw()
+    {
+        _passedTime = millis() - _oldTime;
+        _oldTime = millis();
+        if(_drawTimer <= 0)
+        {
+            _drawTimer = 100;
+            _playButton.draw();
+            _stopButton.draw();
+            _stepLengthSelectButton.draw();
+        }
+        else
+        {
+            _drawTimer = _drawTimer - _passedTime;
+        }
+        if(_stepTimer <= 0)
+        {
+            _stepTimer = _millisToPass;
+            if(_playStatus.isStopped())
+            {
+                _currentStep = 0;
+                _sequencerBarsArea.sendStopped();
+            }
+            if(_playStatus.isPlaying())
+            {
+                _sequencerBarsArea.sendAdvance(_currentStep);
+                _currentStep = _currentStep + 1;
+                if(_currentStep >= STEPS)
+                {
+                    _currentStep = 0;
+                }
+            }
+            background(255);
+            _playButton.draw();
+            _stopButton.draw();
+            _stepLengthSelectButton.draw();
+            _sequencerBarsArea.draw(_currentStep);
+            showCurrentStepAsNumber();
+        }
+        else
+        {
+            _stepTimer = _stepTimer - _passedTime;
+        }
+    }
+    
+    private void showCurrentStepAsNumber()
+    {
+        textFont(_f);
+        textAlign(LEFT);
+        int previousColor = getGraphics().fillColor;
+        fill(0);
+        text(_currentStep, width/2, height/2);
+        fill(previousColor);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event)
+    {
+        _sequencerBarsArea.mousePressed(event, _inputState);
+        _playButton.mousePressed(event, _inputState);
+        _stopButton.mousePressed(event, _inputState);
+        _stepLengthSelectButton.mousePressed(event, _inputState);
+    }
+    
+    public class InputState
+    {
+        private InputStateType _state;
+
+        public void setState(InputStateType newState)
+        {
+            _state = newState;
+        }
+
+        
+        public void stepLengthSelectPressed()
+        {
+            switch (_state)
+            {
+                case STEP_LENGTH_SELECT_ENABLED:
+                    _state = InputStateType.REGULAR;
+                    break;
+                case REGULAR:
+                    _state = InputStateType.STEP_LENGTH_SELECT_ENABLED;
+                default:
+                    break;
+            }
+            System.out.println("new state: " + _state);
+        }
+
+        public void maxStepsSet()
+        {
+            _state = InputStateType.REGULAR;
+        }
+
+        public InputStateType getState()
+        {
+            return _state;
+        }
+    }
+    
+    public enum InputStateType
+    {
+        REGULAR, STEP_LENGTH_SELECT_ENABLED
+    }
+
+    public abstract class SeqButton
+    {
+        protected PApplet _mainApp;
+        protected Rectangle _area;
+        protected PlayStatus _myPlayStatus;
+        protected InputState _myInputState;
+
+        public SeqButton(PApplet mainApp, Rectangle area, PlayStatus playStatus, InputState inputState)
+        {
+            _mainApp = mainApp;
+            _area = area;
+            _myPlayStatus = playStatus;
+            _myInputState = inputState;
+        }
+
+        public void mousePressed(MouseEvent event, InputState inputState)
+        {
+            if(event.getX() > _area.x && event.getX() < _area.x + _area.width && event.getY() > _area.y && event.getY() < _area.y + _area.height)
+            {
+                buttonPressed(inputState);
+            }
+        }
+
+        protected abstract void buttonPressed(InputState inputState);
+
+        public void draw()
+        {
+            int prevCol = _mainApp.getGraphics().fillColor;
+            boolean prevStroke = _mainApp.getGraphics().stroke;
+            int prevStrokeCol = _mainApp.getGraphics().strokeColor;
+            _mainApp.noStroke();
+
+            setColor();
+            _mainApp.rect(_area.x, _area.y, _area.width, _area.height, 10);
+            _mainApp.fill(prevCol);
+            if(prevStroke)
+            {
+                _mainApp.stroke(prevStrokeCol);
+            }
+            else
+            {
+                _mainApp.noStroke();
+            }
+        }
+
+        protected abstract void setColor();
+    }
+
+    public class PlayButton extends SeqButton
+    {
+        public PlayButton(PApplet mainApp, Rectangle area, PlayStatus playStatus, InputState inputState)
+        {
+            super(mainApp, area, playStatus, inputState);
+        }
+
+
+        protected void buttonPressed(InputState inputState)
+        {
+            if(_myPlayStatus.isPlaying())
+            {
+                _myPlayStatus.set(PlayStatus.PAUSED);
+            }
+            else if(_myPlayStatus.isPaused() || _myPlayStatus.isStopped())
+            {
+                _myPlayStatus.set(PlayStatus.PLAYING);
+            }
+        }
+
+        protected void setColor()
+        {
+            if(_myPlayStatus.isPlaying())
+            {
+                _mainApp.fill(32, 128, 64);
+            }
+            else if(_myPlayStatus.isPaused())
+            {
+                _mainApp.fill(16, 64, 32);
+            }
+            else if(_myPlayStatus.isStopped())
+            {
+                _mainApp.fill(8, 32, 16);
+            }
+        }
+    }
+    
+    public class StopButton extends SeqButton
+    {
+        public StopButton(SequencerMain sequencerMain, Rectangle rectangle, PlayStatus playStatus, InputState inputState)
+        {
+            super(sequencerMain, rectangle, playStatus, inputState);
+        }
+
+        @Override
+        protected void buttonPressed(InputState inputState)
+        {
+            if(!_myPlayStatus.isStopped())
+            {
+                _myPlayStatus.set(PlayStatus.STOPPED);
+            }
+        }
+
+        @Override
+        protected void setColor()
+        {
+            _mainApp.fill(32, 32, 32);
+        }
+    }
+
+    public class StepLengthSelectButton extends SeqButton
+    {
+        public StepLengthSelectButton(PApplet mainApp, Rectangle area, PlayStatus playStatus, InputState inputState)
+        {
+            super(mainApp, area, playStatus, inputState);
+        }
+
+        @Override
+        protected void buttonPressed(InputState inputState)
+        {
+            inputState.stepLengthSelectPressed();
+        }
+
+        @Override
+        protected void setColor()
+        {
+            switch (_myInputState._state)
+            {
+                case REGULAR:
+                    _mainApp.fill(128,0,128);
+                    break;
+                case STEP_LENGTH_SELECT_ENABLED:
+                    _mainApp.fill(192,0,192);
+                    break;
+            }
+        }
+    }
+
+    public class PlayStatus
+    {
+        public static final int STOPPED = 0;
+        private static final int PLAYING = 1;
+        private static final int PAUSED = 2;
+
+        private int _status;
+        
+        public PlayStatus(int status)
+        {
+            _status = status;
+        }
+
+        public boolean isPaused()
+        {
+            return _status == PAUSED;
+        }
+
+        public boolean isStopped()
+        {
+            return _status == STOPPED;
+        }
+
+        public int getStatus()
+        {
+            return _status;
+        }
+
+        public boolean isPlaying()
+        {
+            return _status == PLAYING;
+        }
+
+        public void set(int status)
+        {
+            _status = status;
+        }
+    }
+
+    public class SequencerBarArea
+    {
+        private SequencerBar[] _sequencerBars;
+
+        public SequencerBarArea(SequencerMain mainApp, Rectangle area)
+        {
+            PVector insets = new PVector(10, 5);
+            _sequencerBars = new SequencerBar[NUM_TRACKS];
+            _sequencerBars[0] = new SequencerBar(new PVector(area.x, area.y), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[0].draw(0);
+            _sequencerBars[0].setActiveNote(36);
+            _sequencerBars[1] = new SequencerBar(new PVector(area.x, _sequencerBars[0].getDimensions().y + _sequencerBars[0].getDimensions().height), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[1].draw(0);
+            _sequencerBars[1].setActiveNote(38);
+            _sequencerBars[2] = new SequencerBar(new PVector(area.x, _sequencerBars[1].getDimensions().y + _sequencerBars[1].getDimensions().height), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[2].draw(0);
+            _sequencerBars[2].setActiveNote(43);
+            _sequencerBars[3] = new SequencerBar(new PVector(area.x, _sequencerBars[2].getDimensions().y + _sequencerBars[2].getDimensions().height), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[3].draw(0);
+            _sequencerBars[3].setActiveNote(50);
+            _sequencerBars[4] = new SequencerBar(new PVector(area.x, _sequencerBars[3].getDimensions().y + _sequencerBars[3].getDimensions().height), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[4].draw(0);
+            _sequencerBars[4].setActiveNote(42);
+            _sequencerBars[5] = new SequencerBar(new PVector(area.x, _sequencerBars[4].getDimensions().y + _sequencerBars[4].getDimensions().height), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[5].draw(0);
+            _sequencerBars[5].setActiveNote(46);
+            _sequencerBars[6] = new SequencerBar(new PVector(area.x, _sequencerBars[5].getDimensions().y + _sequencerBars[5].getDimensions().height), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[6].draw(0);
+            _sequencerBars[6].setActiveNote(39);
+            _sequencerBars[7] = new SequencerBar(new PVector(area.x, _sequencerBars[6].getDimensions().y + _sequencerBars[6].getDimensions().height), insets, area.width, area.height, STEPS, NUM_TRACKS, STEPS_PER_BEAT, mainApp);
+            _sequencerBars[7].draw(0);
+            _sequencerBars[7].setActiveChannel(0);
+        }
+
+        public void setMidiDeviceBar(MidiDevice midiDevice, int barIdx)
+        {
+            _sequencerBars[barIdx].setMidiDevice(midiDevice);
+        }
+
+        public void sendAdvance(int currentStep)
+        {
+            for(int trackCnt = 0; trackCnt < NUM_TRACKS; trackCnt++)
+            {
+                _sequencerBars[trackCnt].sendAdvance(currentStep);
+            }
+        }
+        
+        public void sendStopped()
+        {
+            for(int trackCnt = 0; trackCnt < NUM_TRACKS; trackCnt++)
+            {
+                _sequencerBars[trackCnt].sendStopped();
+            }
+        }
+
+        public void draw(int currentStep)
+        {
+            for(int trackCnt = 0; trackCnt < NUM_TRACKS; trackCnt++)
+            {
+                _sequencerBars[trackCnt].draw(currentStep);
+            }
+        }
+
+        public void mousePressed(MouseEvent event, InputState inputState)
+        {
+            for(int trackCnt = 0; trackCnt < NUM_TRACKS; trackCnt++)
+            {
+                _sequencerBars[trackCnt].mousePressed(event, inputState);
+            }
+        }
+    }
+
+    public InputState getInputState()
+    {
+        return _inputState;
+    }
+}
